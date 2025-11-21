4ffac6572b0f001240b9e5950114644c8057da46
Crispus Nganga <githogoringash91@gmail.com>
2025-11-21 22:32:02 +0300

    final commit

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 from models import db, User, Movie, Review, WatchlistItem
-from flask_migrate import Migrate
 
 # ---- Flask app ----
 app = Flask(__name__)
@@ -12,8 +11,6 @@ CORS(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", "sqlite:///app.db")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
-migrate = Migrate(app, db)
-
 
 with app.app_context():
     db.create_all()
